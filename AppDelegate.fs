917841c95a47d0ae20a13c34c04f2051ddc5fa3c
Tivas Laursen <tivasl@gmail.com>
2015-03-18 19:31:50 +0100

    Initial Commit of blank mono project

---
+++ AppDelegate.fs
@@ -0,0 +1,23 @@
+﻿namespace BoincThrottler
+
+open System
+open System.Drawing
+open MonoMac.ObjCRuntime
+open MonoMac.Foundation
+open MonoMac.AppKit
+
+[<Register("AppDelegate")>]
+type AppDelegate() = 
+    inherit NSApplicationDelegate()
+    [<DefaultValue>]
+    val mutable mainWindowController : BoincThrottler.MainWindowController
+    override x.FinishedLaunching(notification) = 
+        let mainWindowController = new BoincThrottler.MainWindowController()
+        mainWindowController.Window.MakeKeyAndOrderFront(x)
+
+module main = 
+    [<EntryPoint>]
+    let main args = 
+        NSApplication.Init()
+        NSApplication.Main(args)
+        0
---
+++ MainWindow.fs
@@ -0,0 +1,15 @@
+﻿namespace BoincThrottler
+
+open System
+open MonoMac.ObjCRuntime
+open MonoMac.Foundation
+open MonoMac.AppKit
+
+[<Register("MainWindow")>]
+type MainWindow = 
+    inherit NSWindow
+    new() = { inherit NSWindow() }
+    new(handle : IntPtr) = { inherit NSWindow(handle) }
+    [<Export("initWithCoder:")>]
+    new(coder : NSCoder) = { inherit NSWindow(coder) }
+
